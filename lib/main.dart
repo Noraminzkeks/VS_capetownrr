13fbc3b2c7d1f244a95c07544c641155544ade75
nora <118441096+Noraminzkeks@users.noreply.github.com>
2023-06-10 15:05:16 +0200

    - seetings file added - added language dropdown - added change password button - added log out button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:caperr/settings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -58,20 +59,30 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // Absolutes Grundgerüst
     return Scaffold(
-        appBar: AppBar
-          (
+        appBar: AppBar(
             backgroundColor: const Color(0xFFF3B7BC),
+            title: Text('Cape Town Rent & Ride',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20
+              ),
+            ),
+            leading: Image.asset('assets/images/CapeTownRentRide.jpg',fit: BoxFit.contain,height: 30),
 
-            title: Row
-              (
-              mainAxisAlignment: MainAxisAlignment.start,
-              children:
-              [
-                Image.asset('assets/images/CapeTownRentRide.jpg',fit: BoxFit.contain,height: 30,),
-                Container(padding: const EdgeInsets.all(20.0), child: Text('Cape Town Rent & Ride'))
-              ],
-            )
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings, color: Colors.white),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Settings()),
+                );
+              },
+            ),
+          ],
+
         ),
+
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
